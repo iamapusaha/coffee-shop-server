ac5fc310043fcd242069fabafb6d893db6a365ed
iamapusaha <apusaha392@gmail.com>
2023-10-14 22:44:40 +0600

    setup env again

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const express = require('express');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
-const app = express();
 const cors = require('cors');
+const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(cors())
 app.use(express.json())
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gnqpnrf.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -28,15 +27,15 @@ async function run() {
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-    res.send('welcome to our server')
+    res.send('welcome to your server')
 })
 
 app.listen(port, () => {
-    console.log(`our server is running port: ${port}`);
+    console.log(`our server running port at ${port}`);
 })
